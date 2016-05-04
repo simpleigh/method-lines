2ef9495f03d4c031011e09eacbd0caf53316cf50
Leigh Simpson <code@simpleigh.com>
2016-05-04 22:45:27 +0100

    First go at adding command to generate gsiril proof input

---
+++ lines/commands/gsiril.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+
+import six
+
+from lines.commands import BaseCommand
+
+
+class Command(BaseCommand):
+
+    PLACE_NOTATION_OPTIONS = {
+        'all_dots': True,
+        'external_places': True,
+        'cross_upper_x': True,
+        'symmetry': True,
+        'asymmetric_plus': True,
+    }
+
+    def execute(self):
+        filename = os.path.join(self.get_output_directory(), 'composition.txt')
+        with open(filename, 'w') as file:
+
+            def output(*args):
+                """Prints a line to the output file."""
+                print(*args, file=file)
+
+            # Number of bells: "bells n;"
+            output('{bells} bells;'.format(
+                bells=self.composition.configs.bells,
+            ))
+            output();
+
+            # List of methods with notation for their plain lead
+            for method in six.itervalues(self.composition.configs.methods):
+                output('m_{name}_plain = {notation};'.format(
+                    name=method.name,
+                    notation=method.format(**self.PLACE_NOTATION_OPTIONS),
+                ))
+            output()
+
+            # List of methods omitting the lead end change
+            for method in six.itervalues(self.composition.configs.methods):
+                output('m_{name}_lead = {notation};'.format(
+                    name=method.name,
+                    notation=method.format(
+                        omit_lh=True,
+                        **self.PLACE_NOTATION_OPTIONS
+                    )
+                ))
+            output()
+
+            # List of calls (if these exist)
+            if self.composition.configs.has_config('calls'):
+                for name, call in six.iteritems(self.composition.configs.calls):
+                    output('c_{name} = +{call};'.format(name=name, call=call))
+                output()
+
+            notations = []
+            for lead in self.composition.leads:
+                if lead.call_object is not None:
+                    notations.append('m_{}_lead'.format(lead.method_name))
+                    notations.append('c_{}'.format(lead.call_symbol))
+                else:
+                    notations.append('m_{}_plain'.format(lead.method_name))
+
+            output('prove {parts}('.format(parts=self.composition.parts))
+            output(',\n'.join(notations))
+            output(')')
