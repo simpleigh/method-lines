b6dba571da7657c07feb8eee90be0aec389c6c1a
Leigh Simpson <code@simpleigh.com>
2016-05-13 22:58:39 +0100

    First go at creating setup.py for PyPI package

---
+++ README.rst
@@ -0,0 +1,14 @@
+Method Lines
+============
+
+.. image:: https://travis-ci.org/simpleigh/method-lines.svg?branch=master
+   :alt: Build Status
+   :target: https://travis-ci.org/simpleigh/method-lines
+
+.. image:: https://img.shields.io/github/issues/simpleigh/method-lines.svg
+   :alt: Issues
+   :target: https://github.com/simpleigh/method-lines/issues
+
+Produces method lines and resources for peals in parts.
+
+NOTE: This is very much work-in-progress with some spaghetti sections!
---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup, find_packages
+
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+with open(os.path.join(BASE_DIR, 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+
+with open(os.path.join(BASE_DIR, 'README.rst')) as readme_file:
+    long_description = readme_file.read()
+
+
+with open(os.path.join(BASE_DIR, 'requirements.txt')) as requirements_file:
+    requirements = [line.strip() for line in requirements_file]
+
+
+setup(
+    name='method-lines',
+    version=version,
+    author='Leigh Simpson',
+    author_email='code@simpleigh.com',
+    url='https://github.com/simpleigh/',
+    description='Produces method lines and resources for peals in parts.',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=requirements,
+    scripts=['execute.py'],
+    entry_points={'console_scripts':[
+        'method-lines = method_lines.commands:execute',
+    ]},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    license='GPL',
+)
